f2deee6aebf9cf6d17cf72eb85f493d074ba29c0
Bashir Sanni <bashirsanni4@gmail.com>
2023-11-20 19:07:05 +0000

    Update forms.py

--- bmi_app/forms.py
+++ bmi_app/forms.py
@@ -4,6 +4,4 @@ from .models import BMIRecord
 class BMIForm(forms.ModelForm):
     class Meta:
         model = BMIRecord
-        fields = ['height', 'weight']
-        # fields = ['height', 'weight','code','unit', 'usage','description',
-        #           'valid_until','promoted','add_to_cart','max_items']
+        fields = ['name','height', 'weight']
