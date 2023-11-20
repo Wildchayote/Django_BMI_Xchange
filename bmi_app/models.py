1f161aed1c5777d921e8d55807157e4df44d7ea8
Bashir Sanni <bashirsanni4@gmail.com>
2023-11-20 19:46:06 +0000

    Update models.py

--- bmi_app/models.py
+++ bmi_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class BMIRecord(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(default=False,max_length=120)
     height = models.FloatField(blank=False, null=True)
     weight = models.FloatField(blank=False, null=True)
     BMI = models.FloatField(blank=False, null=True)
