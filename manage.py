06709f4f85c3c04338b38451a6f5605bbc63a73b
Bashir Sanni <bashirsanni4@gmail.com>
2023-11-20 18:10:41 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+import pathlib
 
 
 def main():
     """Run administrative tasks."""
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmi_project.settings')
     try:
         from django.core.management import execute_from_command_line
